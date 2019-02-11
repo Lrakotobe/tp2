4d8cd0cecd9b1ca7f682176624ba2beace218b61
Lrakotobe <47389095+Lrakotobe@users.noreply.github.com>
2019-02-11 23:48:36 +0100

    Add files via upload

--- tp2.js
+++ tp2.js
@@ -1,20 +1,43 @@
-var people= []
-var table= document.createElement("table");
-var line= document.createElement("tr");
-var cell= document.createElement("td");
-cell.innerHTML=console.log(person);
-for (i=0; i<2; i++){
-	var person= {
-		Last: faker.name.lastName(),
-		First: faker.name.firstName(),
-		Age: randRange(18,100),
-		Job: faker.lorem.words(),
-		Tel: faker.phone.phoneNumber(),
-		Adress: faker.address.city(),
+window.addEventListener("load", main);
+var people = [];
+var table= document.createElement('table');
+var line= document.createElement('tr');
+var cell= document.createElement('td');
+
+function main(event){
+	document.write("<table>");
+	document.write("<tr>");
+	document.write("<td>Last</td><td>First</td><td>Age</td><td>Job</td><td>Tel</td><td>Adress</td>");
+	document.write("<tr>");
+	for (var i=0; i<50; i++){
+		var person= {
+			Last: faker.name.lastName(),
+			First: faker.name.firstName(),
+			Age: randRange(18,100),
+			Job: faker.lorem.words(),
+			Tel: faker.phone.phoneNumber(),
+			Adress: faker.address.city(),
+		}	
+		people.push(person);
+	}
+	document.write("<tr>");
+   	document.write("<td>" + console.table(people) + "</td>");
+   	document.write("</tr>");
+    document.write("</table>");
+}
+
+function tab(){
+	 for (var i = 0; i < people.length; i++){
+		table.appendChild(line);
+		line.appendChild(cell);
+		cell.innerHTML= main;
+	document.body.appendChild(table);
 	}
 }
+
 
 
 function randRange(min, max){
 	return Math.floor(Math.random() * (max-min+1)) +min;
 }
+
